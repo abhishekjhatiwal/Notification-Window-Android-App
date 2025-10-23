540ea85cf186329de68f041eae3f83821dd956b9
Abhishekverma2006 <j472812@gmail.com>
2025-10-23 18:03:31 +0530

    Third commit

--- app/src/main/java/com/example/notificationwindow/NotificationHelper.kt
+++ app/src/main/java/com/example/notificationwindow/NotificationHelper.kt
@@ -5,9 +5,7 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
